1ea6a745d0722ff289eafa839681f565f2eb7db8
Michael Strasser <mstrass@thoughtworks.com>
2023-05-17 21:22:09 +1000

    Shorter lengths for address fields

--- src/main/kotlin/mjs/data/Persistence.kt
+++ src/main/kotlin/mjs/data/Persistence.kt
@@ -7,8 +7,8 @@ class Persistence {
     fun saveAddress(address: Address) {
 
         assert(address.id in (1_000_000..1_000_000_000_000))
-        assert(address.street.length in (2..100))
-        assert(address.suburb.length in (2..50))
+        assert(address.street.length in (2..60))
+        assert(address.suburb.length in (2..30))
         assert(address.postcode in (1000..9000))
 
         addresses[address.id] = address
